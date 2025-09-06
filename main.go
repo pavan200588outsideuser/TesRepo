99ce09b13f8d175403e9d5bc277f120d140accca
pavan <pavan200588@gmail.com>
2025-09-06 22:41:48 +0530

    t3

--- main.go
+++ main.go
@@ -1,21 +1,23 @@
 package main
 
-import "fmt"
-
-func plus(a int, b int) int {
-
-    return a + b
-}
-
-func plusPlus(a, b, c int) int {
-    return a + b + c
-}
+import (
+	"crypto/tls"
+	"log"
+	"net/http"
+)
 
 func main() {
 
-    res := plus(1, 2)
-    fmt.Println("1+2 =", res)
+	server := http.Server{
+		Addr: "0.0.0.0:8080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("success"))
+		}),
+	}
+	server.TLSConfig = &tls.Config{MinVersion: tls.VersionTLS10}
+	if err := server.ListenAndServeTLS("server.crt", "server.key"); err != nil {
+		log.Fatalf("server crashed :: %s", err.Error())
+	}
 
-    res = plusPlus(1, 2, 3)
-    fmt.Println("1+2+3 =", res)
 }
